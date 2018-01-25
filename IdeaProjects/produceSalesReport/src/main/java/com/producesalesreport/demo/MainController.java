c8d561fc9676201aa108d143323d6064bcd23778
NatashaLBallard <nbitcareer@gmail.com>
2018-01-25 10:42:06 -0500

    updated code to calculate sales total, added better formatting

--- IdeaProjects/produceSalesReport/src/main/java/com/producesalesreport/demo/MainController.java
+++ IdeaProjects/produceSalesReport/src/main/java/com/producesalesreport/demo/MainController.java
@@ -1,4 +1,4 @@
-package print_grades.demo;
+package com.producesalesreport.demo;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,28 +10,39 @@ import java.util.Scanner;
 public class MainController {
 
     @RequestMapping ("/")
-    public String print_grade()
-    {
+    public String producesalesreport() {
 
-        int cusNumber = 0;
-        String cusName = "";
-        int salesAmount = 0;
+        int cusNumber = 0123;
+        String cusName = "Jane Doe";
+        double salesAmount = 30.56;
 
+        double taxCode = 0;
         String taxCode0 = "tax exempt";
-        int taxCode1 = .03;
-        int taxCode2 = .05;
-        int salesAmount = hhh;
 
-        if 
+        double taxCode1 = .03;
+        double taxCode2 = .05;
+        double salesTotal = (salesAmount * taxCode) + salesAmount;
+
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter test score.");
-        testScore = keyboard.nextInt();
+        System.out.println("Enter in tax code:");
+        taxCode = keyboard.nextDouble();
+
+        if (taxCode == 0) {
+            System.out.println(taxCode0);
+        }
+        else if (taxCode == 1) {
+            salesTotal = (salesAmount * taxCode1) + salesAmount;
+        }
+        else if (taxCode == 2) {
+            salesTotal = (salesAmount * taxCode2) + salesAmount;
+        }
 
 
-        String displayGrade = "Sales Report" + '\n' + cusNumber + cusName + salesAmount;
+
+        String displayReport = "<b>"+ "Sales Report" +"</b>" + "<p>" + "Customer Number: " + cusNumber + "<p>" + "Customer Name: " + cusName + "<p>" + "Sales Amount: " + salesAmount + "<p>" + "Sales Total: " + salesTotal;
 
         System.out.println();
-        return displayGrade;
+        return displayReport;
 
     }
 
